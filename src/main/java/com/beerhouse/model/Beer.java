072f493e605caeda68d8c8eef2bed1088fb64792
Rodrigo Tenorio de BOa Ventura <boaventura19@yahoo.com.br>
2019-10-02 18:03:51 -0300

    criacao da classe modelo Beer.

---
+++ src/main/java/com/beerhouse/model/Beer.java
@@ -0,0 +1,106 @@
+package com.beerhouse.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Beer implements Serializable {
+	@JsonProperty("id")
+	private Integer id = null;
+
+	@JsonProperty("name")
+	private String name = null;
+
+	@JsonProperty("ingredients")
+	private String ingredients = null;
+
+	@JsonProperty("alcoholContent")
+	private String alcoholContent = null;
+
+	@JsonProperty("price")
+	private BigDecimal price = null;
+
+	@JsonProperty("category")
+	private String category = null;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(String ingredients) {
+		this.ingredients = ingredients;
+	}
+
+	public String getAlcoholContent() {
+		return alcoholContent;
+	}
+
+	public void setAlcoholContent(String alcoholContent) {
+		this.alcoholContent = alcoholContent;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Beer other = (Beer) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Beer [id=" + id + ", name=" + name + ", ingredients=" + ingredients + ", alcoholContent="
+				+ alcoholContent + ", price=" + price + ", category=" + category + "]";
+	}
+
+}
